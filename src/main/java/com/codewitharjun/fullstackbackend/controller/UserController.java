f4ea50369aa195e8bd75e3843f50b4ba9d9e1de6
zukahm2005 <145833253+zukahm2005@users.noreply.github.com>
2024-06-16 21:15:20 +0700

    aaa

--- src/main/java/com/codewitharjun/fullstackbackend/controller/UserController.java
+++ src/main/java/com/codewitharjun/fullstackbackend/controller/UserController.java
@@ -51,7 +51,16 @@ public class UserController {
         userRepository.deleteById(id);
         return  "User with id "+id+" has been deleted success.";
     }
-
-
+    @PutMapping("/user/{id}")
+    User deleteUser(@RequestBody User newUser, @PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(newUser.getUsername());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                }).orElseThrow(() -> new UserNotFoundException(id));
+    }
+//test pust github
 
 }
